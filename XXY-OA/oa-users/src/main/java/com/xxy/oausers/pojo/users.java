87735bb4e45e7ffb067d06a40bd34ff64de0c197
AuraroCloud <simeilong2021@163.com>
2022-05-10 15:42:35 +0800

    01用户实体类的创建

---
+++ XXY-OA/oa-users/src/main/java/com/xxy/oausers/pojo/users.java
@@ -0,0 +1,55 @@
+package com.xxy.oausers.pojo;
+
+import java.util.Date;
+
+/**
+ * creat by sml
+ * 2022-05-10
+ **/
+public class users {
+    private String user_id;
+    private String user_name;
+    private String login_name;
+    private String password;
+    private Date creat_time;
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getLogin_name() {
+        return login_name;
+    }
+
+    public void setLogin_name(String login_name) {
+        this.login_name = login_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreat_time() {
+        return creat_time;
+    }
+
+    public void setCreat_time(Date creat_time) {
+        this.creat_time = creat_time;
+    }
+}

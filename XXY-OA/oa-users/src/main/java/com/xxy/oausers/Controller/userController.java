e7193acc578f275ec9a97bb0b6ac97b2719f8b84
AuraroCloud <simeilong2021@163.com>
2022-05-10 17:16:03 +0800

    用户增删改查mapper

---
+++ XXY-OA/oa-users/src/main/java/com/xxy/oausers/Controller/userController.java
@@ -0,0 +1,13 @@
+package com.xxy.oausers.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * creat by sml
+ * 2022-05-10
+ **/
+@RequestMapping
+public class userController  {
+
+
+}
---
+++ XXY-OA/oa-users/src/main/java/com/xxy/oausers/mapper/userMapper.java
@@ -0,0 +1,20 @@
+package com.xxy.oausers.mapper;
+
+import com.xxy.oausers.pojo.users;
+
+/**
+ * creat by sml
+ * 2022-05-10
+ **/
+public interface userMapper {
+    //增加一个用户
+    int insertUser();
+    //删除一个用户
+    int   deleteUser();
+    //查询一个用户
+    users selectUser();
+    //修改用户的信息
+    int updateUser();
+
+
+}
